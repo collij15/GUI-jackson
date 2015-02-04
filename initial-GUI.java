38a44859a923f828eff594db62b884b4d328e3d8
collij15 <collij15@polyprep.org>
2015-02-04 12:50:53 -0500

    Initial Upload

---
+++ initial-GUI.java
@@ -0,0 +1,16 @@
+public class GUI
+{
+    public static void main(String [] args)
+    {
+        double timeUntilTest = 200000.0;
+        int weeks;
+        int days;
+        int hours;
+        int minutes;
+        int seconds;
+        
+        seconds = timeUntilTest;
+        minutes = timeUntilTest / 60;
+        seconds = seconds % 60
+    }
+}
